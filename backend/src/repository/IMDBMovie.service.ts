1495e62bfbdde7fe5df8fc0ff8b4d7392e5f9834
Maurice Reigman <m_c.r@hotmail.com>
2018-03-08 13:06:21 +0100

    Update IMDBMovie.service.ts

--- backend/src/repository/IMDBMovie.service.ts
+++ backend/src/repository/IMDBMovie.service.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosPromise } from "axios";
 
 export class IMDBMovieService {
-    public static readonly OMDBAPIKEY = "e36ea2a2";
+    public static readonly OMDBAPIKEY = "blabla";
     public static readonly OMDBAPI = `http://www.omdbapi.com/?apikey=${IMDBMovieService.OMDBAPIKEY}&`;
 
     public static movies(paramString: string): AxiosPromise<IIMDBMoviesResponse> {
